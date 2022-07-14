19c440c412c2891fd233c03c41cfbfc6f1e2a63c
Frances Stewart <fstewart@wlu.ca>
2022-07-14 14:18:44 +0000

    initial commits

---
+++ Data manipulation with dplyr/ClassCentral_dplyrTutorial.R
@@ -0,0 +1,12 @@
+#################################
+# Frances Stewart
+# https://www.classcentral.com/course/youtube-data-manipulation-in-r-with-dplyr-92296/classroom
+# Skills tune-up workshop
+## Hands-on dplyr tutorial for R
+## data exploration and manipulation focused on dataframes
+#################################
+
+# Watch the follow-up tutorial: http://youtu.be/2mh1PqfsXVI
+# Rmd document: https://rpubs.com/justmarkham/dplyr-tutorial
+## Work off of this document for the course. 
+# GitHub repository: https://github.com/justmarkham/dplyr-tutorial
